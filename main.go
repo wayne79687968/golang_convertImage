6b0d218c9f491448abeee45589db974a5ec07d6f
Skogkatt <wayne79687968@gmail.com>
2023-02-15 15:34:34 +0800

    Fix: open file and close file

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -12,5 +14,15 @@ func main() {
 	if err != nil {
 		fmt.Println("Read file error:", err)
 		return
+	}
+
+	for _, file := range files {
+		infile, err := os.Open(filepath.Join(inputDir, file.Name()))
+		if err != nil {
+			fmt.Printf("Open file %s error：%s\n", file.Name(), err)
+			return
+		}
+		defer infile.Close()
+
 	}
 }
