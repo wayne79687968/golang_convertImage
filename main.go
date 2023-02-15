f7c1ae47c35f2f2ccb807da299593e93086cb71d
Skogkatt <wayne79687968@gmail.com>
2023-02-15 15:38:17 +0800

    Fix: create output file

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
 	inputDir := "./input"
+	outputDir := "./output"
 
 	files, err := ioutil.ReadDir(inputDir)
 	if err != nil {
@@ -24,5 +26,12 @@ func main() {
 		}
 		defer infile.Close()
 
+		outFileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name())) + ".jpg"
+		outfile, err := os.Create(filepath.Join(outputDir, outFileName))
+		if err != nil {
+			fmt.Printf("Create file %s error：%s\n", outFileName, err)
+			return
+		}
+		defer outfile.Close()
 	}
 }
